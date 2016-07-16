d519cdab964a39d9b93fd8495645b59ae7404b71
Skiff Chen <skiffc@gmail.com>
2016-07-16 16:16:15 +0800

    Re-run compatible Add Error handling

--- do_rename.py
+++ do_rename.py
@@ -4,14 +4,34 @@ import  re
 import  sys
 import  exifread
 
+fail = []
+
 for root, folders, files in os.walk( sys.argv[1]):
     if root != sys.argv[1]:
         continue
     for f in files:
         if os.path.splitext( f )[1] in ( '.db' ):
             continue
-        tags = exifread.process_file( open( os.path.join( root, f), 'rb' ) )
-        r = re.sub( ':', '', str(tags['EXIF DateTimeOriginal']).split()[0] ) + '_' + f 
-        print f, r 
-        os.rename( os.path.join( root, f), os.path.join( root, r ) )
+        if re.search( '^20\d{6}$', f.split('_')[0] ):
+            s = f.split('_')
+            continue
 
+        tags = exifread.process_file( open( os.path.join( root, f), 'rb' ) )
+        try:
+            r = re.sub( ':', '', str(tags['EXIF DateTimeOriginal']).split()[0] ) + '_' + f 
+            print f, r 
+            os.rename( os.path.join( root, f), os.path.join( root, r ) )
+        except:
+            print '[ERROR]', f
+            for t in tags:
+                if 'DateTime' in t:
+                    print '  - tag:', t, '->', tags[t]
+            print tags.keys()
+             
+            fail.append( f )
+
+if fail:
+    print 'FAIL:'
+    for p in fail:
+        print p
+   
